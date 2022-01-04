f787f95df7409493e220e8212e1e0f4f5b538ddb
Akash patel <akashpatel10000@gmail.com>
2022-01-04 22:56:51 +0530

    Made initial changes and setup file structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Register from './Pages/Register/Register';
+import styled from 'styled-components';
+import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Register/>}/>
+        </Routes>
+      </Router>
+    </Container>
   );
 }
+// Styling
+const Container = styled.div`
 
-export default App;
+`;
---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Register() {
+    return (
+        <Container>
+            <h2>Registration page</h2>
+        </Container>
+    )
+}
+// Styling
+const Container = styled.div`
+
+`;
