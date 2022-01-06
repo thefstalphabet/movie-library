56c50dd58e2db112e8750278598c0f57b36074cb
Akash patel <akashpatel10000@gmail.com>
2022-01-06 19:27:40 +0530

    Created google auth login and logout functionality and created search functionality

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,14 +1,63 @@
-import React from 'react';
-import styled from 'styled-components';
+import { React, useState } from "react";
+import styled from "styled-components";
+import { auth } from "../../firebase";
+import { Navigate } from "react-router-dom";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 export default function Register() {
-    return (
-        <Container>
-            <h2>Registration page</h2>
-        </Container>
-    )
+  // Sign in with google function
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((response) => {
+        console.log(response);
+        localStorage.setItem("user", "true");
+        localStorage.setItem("user_info", JSON.stringify(response.user));
+        window.location.reload(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <Container>
+      {localStorage.getItem("user") === "true" && <Navigate to="/dashboard" />}
+      <h1>Welcome to the Movie Library</h1>
+      <Button onClick={signInWithGoogle}>
+        <img src="/Assets/google-icon.svg" alt="google-icon" />
+        <span>Sign In with Google</span>
+      </Button>
+    </Container>
+  );
 }
 // Styling
 const Container = styled.div`
-
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  top: 250px;
+  h1 {
+    font-size: 25px;
+    font-weight: 600;
+  }
+`;
+const Button = styled.button`
+  border: none;
+  outline: none;
+  align-items: center;
+  min-width: 20%;
+  padding: 10px;
+  margin-top: 30px;
+  font-size: 14px;
+  font-weight: 600;
+  background-color: #f0f0f0;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  span {
+    margin-left: 10px;
+  }
 `;
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,20 @@
 // Imports that we need from firebase
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // Our web app firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBBigD-5Kc8T7XNJ9cCFvH3JzaIcDY1Mi8",
-  authDomain: "movie-library-61aa4.firebaseapp.com",
-  projectId: "movie-library-61aa4",
-  storageBucket: "movie-library-61aa4.appspot.com",
-  messagingSenderId: "979439936491",
-  appId: "1:979439936491:web:d2c636e946031cd9d92fae",
-  measurementId: "G-26NRRNR5KK",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASURMENT_ID,
 };
 
 // Initilalize the firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
